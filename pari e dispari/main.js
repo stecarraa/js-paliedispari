759ade0cd52e2ad26e2fa798982d5183d14a3a43
stecarraa <stefano@caratozzolo.me>
2022-06-22 17:09:25 +0200

    ok pari e dispari, no controlli

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -4,3 +4,46 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+let choice = prompt('scegli pari o dispari') .toUpperCase();
+console.log(choice)
+
+const userNumber = parseInt(prompt('inserisci un numero da 1 a 5'))
+console.log(userNumber)
+
+const pcNumber = randomNumber(1,5)
+console.log(pcNumber)
+
+
+
+
+sum = userNumber + pcNumber
+console.log(sum)
+
+if (((choice === "PARI") && (isEven(sum) === true)) || ((choice === "DISPARI") && (isEven(sum) === false ))){
+    console.log(`vince l'utente`);
+} else {
+    console.log(`vince il calcolatore`);
+} 
+
+
+
+
+
+
+
+function randomNumber (min,max){
+    return Math.floor(Math.random()*(max - min) + min);
+}
+
+function isEven (number){
+    
+    if(number % 2 == 0){
+     return true
+    }
+    else{
+        return false
+    }
+    
+}
+
